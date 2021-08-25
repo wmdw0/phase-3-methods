c9c9e1dbb8f3d85d0d4a4e6f734bd38175c2012e
William Walsh <wmdwalsh0@gmail.com>
2021-08-25 18:54:19 -0400

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num1)
+    if num1.is_a? Integer 
+        return num1 / 2
+    else
+        return nil
+    end
+end 
